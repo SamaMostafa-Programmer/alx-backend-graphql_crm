efa230f1a78d32cd6319a1cb54a52b12ca844f70
Sama Mostafa <samam6778@gmail.com>
2026-01-11 20:44:40 +0200

    Update cron.py

--- crm/cron.py
+++ crm/cron.py
@@ -30,3 +30,34 @@ def log_crm_heartbeat():
     timestamp = datetime.now().strftime("%d/%m/%Y-%H:%M:%S")
     with open("/tmp/crm_heartbeat_log.txt", "a") as f:
         f.write(f"{timestamp} CRM is alive\n")
+
+from gql import gql, Client
+from gql.transport.requests import RequestsHTTPTransport
+from datetime import datetime
+
+def update_low_stock():
+    transport = RequestsHTTPTransport(
+        url="http://localhost:8000/graphql",
+        verify=True,
+        retries=3,
+    )
+
+    client = Client(transport=transport, fetch_schema_from_transport=True)
+
+    mutation = gql("""
+    mutation {
+      updateLowStockProducts {
+        success
+        products {
+          name
+          stock
+        }
+      }
+    }
+    """)
+
+    result = client.execute(mutation)
+
+    with open("/tmp/low_stock_updates_log.txt", "a") as f:
+        for p in result["updateLowStockProducts"]["products"]:
+            f.write(f"{datetime.now()} - {p['name']} new stock: {p['stock']}\n")
