ec880691ec3c4150cc603780b4d5a8591b936100
Sama Mostafa <samam6778@gmail.com>
2026-01-11 20:39:35 +0200

    Add CRM heartbeat logging function

---
+++ crm/cron.py
@@ -0,0 +1,6 @@
+from datetime import datetime
+
+def log_crm_heartbeat():
+    timestamp = datetime.now().strftime("%d/%m/%Y-%H:%M:%S")
+    with open("/tmp/crm_heartbeat_log.txt", "a") as f:
+        f.write(f"{timestamp} CRM is alive\n")
