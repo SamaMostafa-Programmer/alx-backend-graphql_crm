096515358e1a73de65937829f4038e9f3706c78a
Sama Mostafa <samam6778@gmail.com>
2026-01-11 20:49:30 +0200

    Add Celery configuration for scheduled tasks

--- crm/settings.py
+++ crm/settings.py
@@ -5,3 +5,15 @@ INSTALLED_APPS = [
 CRONJOBS += [
     ('0 */12 * * *', 'crm.cron.update_low_stock'),
 ]
+INSTALLED_APPS += ['django_celery_beat']
+
+from celery.schedules import crontab
+
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
+
+CELERY_BEAT_SCHEDULE = {
+    'generate-crm-report': {
+        'task': 'crm.tasks.generate_crm_report',
+        'schedule': crontab(day_of_week='mon', hour=6, minute=0),
+    },
+}
