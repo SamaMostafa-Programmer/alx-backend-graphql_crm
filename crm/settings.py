016554a836df15dcb270e3ad59ce735330fde692
Sama Mostafa <samam6778@gmail.com>
2026-01-11 20:45:47 +0200

    Modify CRONJOBS for low stock updates
    Updated CRONJOBS to include low stock update job.

--- crm/settings.py
+++ crm/settings.py
@@ -2,7 +2,6 @@ INSTALLED_APPS = [
     ...
     'django_crontab',
 ]
-
-CRONJOBS = [
-    ('*/5 * * * *', 'crm.cron.log_crm_heartbeat'),
+CRONJOBS += [
+    ('0 */12 * * *', 'crm.cron.update_low_stock'),
 ]
